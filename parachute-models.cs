2c14fdd4f95c58fbece1c8e3488ffc174478375a
cmaldo16 <cmaldo16@gmail.com>
2022-10-13 15:40:30 -0600

    made model_specs class and made model variables

--- parachute-models.cs
+++ parachute-models.cs
@@ -1,3 +1,11 @@
 ﻿namespace jumper {
-    
+    class model_specs {
+        private void models() {
+            string full_parachute;
+            string one_wrong;
+            string two_wrong;
+            string three_wrong;
+            string deceased_jumper;
+        }
+    }
 }
