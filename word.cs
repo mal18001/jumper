75709dee0db1c494657c4e9cdf91438127c3d700
aiden-patterson <pat18001@byui.edu>
2022-10-18 16:24:29 -0600

    random word generator added

--- word.cs
+++ word.cs
@@ -2,12 +2,36 @@ namespace jumper.Game
 {
     public class Word
     {
-        private String randomWord;
+        private String goal_word;
         private String playerGuess;
+        Random randomGenerator = new Random();
+
 
         public Word(){
             playerGuess = "";
-            randomWord = "";
+            int index = randomGenerator.Next(0, 10);
+
+            List<string> words = new List<string>();
+
+            words.Add("giraffe");
+            words.Add("cantaloupe");
+            words.Add("prowler");
+            words.Add("hurricane");
+            words.Add("precipice");
+            words.Add("poltergeist");
+            words.Add("exponential");
+            words.Add("sagebrush");
+            words.Add("gazebo");
+            words.Add("liberty");
+
+            goal_word = words[index];
+
+            int length = goal_word.Length;
+
+            foreach (char c in goal_word)
+            {
+                Console.Write(c.ToString());
+            }
         }
 
         public void setPlayerGuess(String guess){
